55cdbde22642d3a93c0945b299e7a45fd631fb60
joypauls <joeypaulsen3@gmail.com>
2020-05-10 18:22:39 -0500

    random progress

--- src/App.js
+++ src/App.js
@@ -34,6 +34,20 @@ const theme = {
       height: '20px',
     },
   },
+  rangeInput: {
+    track: {
+      color: "rgb(196, 196, 196)",
+      height: "5px",
+      extend: () => `border-radius: 10px; border-color: #000000; border-width: 2px`
+    },
+    thumb: {
+      color: "rgb(196, 196, 196)",
+      width: "8px",
+      extend: () => `height: 10px`
+      // borderColor: "rgb(196, 196, 196)",
+      // borderWidth: "3px"
+    }
+  },
 };
 
 function MadeWithLove() {
@@ -45,6 +59,18 @@ function MadeWithLove() {
   );
 }
 
+
+function PaletteElement(props) {
+  return (
+    <Box>
+      <SliderInput value={props.color1.r} setValue={props.color1.setR} />
+      <SliderInput value={props.color1.g} setValue={props.color1.setG} />
+      <SliderInput value={props.color1.b} setValue={props.color1.setB} />
+      <Box elevation="medium" background={makeRGBString(props.color1.r, props.color1.g, props.color1.b)} width="medium" height="medium">Item 1</Box>
+    </Box>
+  )
+}
+
 function Palette(props) {
   return (
     <Grid
@@ -54,7 +80,7 @@ function Palette(props) {
       }}
       gap="small"
     >
-      <Box elevation="medium" background={makeRGBString(props.r, props.g, props.b)} width="medium" height="medium">Item 1</Box>
+      <PaletteElement {...props} />
       <Box elevation="medium" background="brand" width="medium" height="medium">Item 2</Box>
       <Box elevation="medium" background="brand" width="medium" height="medium">Item 3</Box>
     </Grid>
@@ -66,11 +92,25 @@ function SliderInput(props) {
     <RangeInput
       min={0}
       max={255}
+      step={1}
       value={props.value}
       onChange={event => props.setValue(event.target.value)}
     />
   );
 }
+
+
+class Color {
+  constructor(r, g, b, setR, setG, setB) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.setR = setR;
+    this.setG = setG;
+    this.setB = setB;
+  }
+}
+
 
 function makeRGBString(r, g, b) {
   return (
@@ -99,46 +139,48 @@ function App() {
   const [valueG, setValueG] = useState(10);
   const [valueB, setValueB] = useState(10);
 
+  const color1 = new Color(valueR, valueG, valueB, setValueR, setValueG, setValueB);
+
+
   return (
     <Grommet theme={theme} full>
-
-    <Box fill>
-    <AppBar>
-      <Heading level='3' margin='none'>color & conjury &#10024; &#128142; &#128302;</Heading>
-      <Button
-           icon={<Configure />}
-           onClick={() => setShowSidebar(!showSidebar)}
-         />
-    </AppBar>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar>
+              <Heading level='3' margin='none'>color & conjury &#10024; &#128142; &#128302;</Heading>
+              <Button
+                icon={<Configure />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
     
-    <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-      <Box flex align='center' justify='center'>
-      <Text>{ valueR }, { valueG }, { valueB }</Text>
-        <Palette r={valueR} g={valueG} b={valueB} />
-      </Box>
-        <Collapsible direction="horizontal" open={showSidebar}>
-           <Box
-           flex
-             width='medium'
-             pad="small"
-             background='light-2'
-             elevation='small'
-             align='center'
-             justify='center'
-           >
-             <SliderInput value={valueR} setValue={setValueR} />
-             <SliderInput value={valueG} setValue={setValueG} />
-             <SliderInput value={valueB} setValue={setValueB} />
-           </Box>
-           </Collapsible>
-    </Box>
-  </Box>
-
-      <Footer pad="medium">
-        {/* <Container maxWidth="sm"> */}
-          <MadeWithLove />
-        {/* </Container> */}
-        </Footer>
+            <Box direction='row' flex >
+              <Box flex align='center' justify='center'>
+                <Text>{ color1.r }, { color1.g }, { color1.b }</Text>
+                <Palette color1={color1} />
+              </Box>
+              {size !== 'small' && (
+                <Collapsible direction="horizontal" open={showSidebar}>
+                  <Box
+                  flex
+                  width='medium'
+                  pad="small"
+                  background='light-2'
+                  elevation='small'
+                  align='center'
+                  justify='center'
+                  >
+                  </Box>
+                </Collapsible>
+              )}
+            </Box>
+            <Footer pad="small" justify="center">
+              <MadeWithLove />
+            </Footer>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
     </Grommet>
   );
 }
